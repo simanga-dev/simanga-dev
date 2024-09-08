4c4e41928b0470c4d548a0e704651068707aa6b2
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-09-08 09:10:17 +0000

    chore(setup): setup drizzy and lucia auth

---
+++ auth/db/schema.ts
@@ -0,0 +1,18 @@
+// import pg from "pg";
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+export const userTable = pgTable("user", {
+  id: text("id").primaryKey(),
+});
+
+export const sessionTable = pgTable("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+});
---
+++ auth/lib/auth.ts
@@ -0,0 +1,32 @@
+import { Lucia } from "lucia";
+
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { sessionTable, userTable } from "@/db/schema";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      username: attributes.username,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
+}
