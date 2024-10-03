76fe568c27bda87b31b207912062fb4b02eb597c
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-10-03 11:40:45 +0200

    feat(work-in-progress): work in progress for auth with lucia

--- auth/db/schema.ts
+++ auth/db/schema.ts
@@ -1,29 +1,32 @@
-// import pg from "pg";
 import {
-  text,
-  timestamp,
-  integer,
-  boolean,
-  pgTable,
+    text,
+    timestamp,
+    integer,
+    boolean,
+    pgTable,
 } from "drizzle-orm/pg-core";
 
 export const userTable = pgTable("user", {
-  id: text("id").primaryKey(),
+    id: text("id").primaryKey(),
+    email: text("email").notNull().unique(),
+    first_name: text("first_name"),
+    last_name: text("last_name"),
+    password_hash: text("password_hash").notNull()
 });
 
 export const sessionTable = pgTable("session", {
-  id: text("id").primaryKey(),
-  userId: text("user_id")
-    .notNull()
-    .references(() => userTable.id),
-  expiresAt: timestamp("expires_at", {
-    withTimezone: true,
-    mode: "date",
-  }).notNull(),
+    id: text("id").primaryKey(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => userTable.id),
+    expiresAt: timestamp("expires_at", {
+        withTimezone: true,
+        mode: "date",
+    }).notNull(),
 });
 
 export const todo = pgTable("todo", {
-  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-  text: text("text").notNull(),
-  done: boolean("done").default(false).notNull(),
+    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+    text: text("text").notNull(),
+    done: boolean("done").default(false).notNull(),
 });
--- auth/lib/auth.ts
+++ auth/lib/auth.ts
@@ -7,27 +7,27 @@ import { db } from "@/db";
 const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
 
 export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    expires: false,
-    attributes: {
-      secure: process.env.NODE_ENV === "production",
+    sessionCookie: {
+        expires: false,
+        attributes: {
+            secure: process.env.NODE_ENV === "production",
+        },
     },
-  },
-  getUserAttributes: (attributes) => {
-    return {
-      // attributes has the type of DatabaseUserAttributes
-      username: attributes.username,
-    };
-  },
+    getUserAttributes: (attributes) => {
+        return {
+            // attributes has the type of DatabaseUserAttributes
+            username: attributes.username,
+        };
+    },
 });
 
 declare module "lucia" {
-  interface Register {
-    Lucia: typeof lucia;
-    DatabaseUserAttributes: DatabaseUserAttributes;
-  }
+    interface Register {
+        Lucia: typeof lucia;
+        DatabaseUserAttributes: DatabaseUserAttributes;
+    }
 }
 
 interface DatabaseUserAttributes {
-  username: string;
+    username: string;
 }
