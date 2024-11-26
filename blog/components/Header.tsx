e9841677b0d8ce86b1234a4513135c8521374cd4
Simanga Khoza <simanga-dev@users.noreply.github.com>
2024-11-26 19:50:54 +0200

    style(logo): update logo

--- blog/components/Header.tsx
+++ blog/components/Header.tsx
@@ -16,12 +16,12 @@ const Header = () => {
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
-          <div className="">
+          <div className="scale-40">
             <Logo />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle.substring(2)}
+              {siteMetadata.headerTitle}
             </div>
           ) : (
             siteMetadata.headerTitle
